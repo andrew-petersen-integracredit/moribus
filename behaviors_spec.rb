bb458f3e1326a1400af306c18f229312aa1241d3
Artem Kuzko <a.kuzko@gmail.com>
2012-03-07 21:23:34 -0600

    Moved behaviors_spec.rb file

---
+++ behaviors_spec.rb
@@ -0,0 +1,174 @@
+require 'spec_helper'
+
+describe Core::Behaviors do
+  before :all do
+    class SpecStatus < SpecModel(:name => :string, :description => :string)
+      acts_as_enumerated
+
+      self.enumeration_model_updates_permitted = true
+      create!(:name => 'inactive', :description => 'Inactive')
+      create!(:name => 'active', :description => 'Active')
+    end
+
+    class SpecSuffix < SpecModel(:name => :string, :description => :string)
+      acts_as_enumerated
+
+      self.enumeration_model_updates_permitted = true
+      create!(:name => 'none', :description => '')
+      create!(:name => 'jr', :description => 'Junior')
+    end
+
+    class SpecPersonName < SpecModel(:first_name => :string, :last_name => :string, :spec_suffix_id => :integer)
+      acts_as_aggregated
+      has_enumerated :spec_suffix, :default => ''
+
+      validates_presence_of :first_name, :last_name
+    end
+
+    class SpecCustomerInfo < SpecModel(:spec_customer_id => :integer!, :spec_person_name_id => :integer, :spec_status_id => :integer, :is_current => :boolean)
+      attr :custom_field
+
+      belongs_to :spec_customer, :inverse_of => :spec_customer_info, :touch => true
+      has_aggregated :spec_person_name
+      has_enumerated :spec_status
+
+      acts_as_tracked
+    end
+
+    class SpecCustomer < SpecModel(:id => :integer, :spec_status_id => :integer)
+      has_one_current :spec_customer_info, :inverse_of => :spec_customer
+      has_enumerated :spec_status, :default => 'inactive'
+
+      delegate_associated :spec_person_name, :custom_field, :to => :spec_customer_info
+
+      share :spec_status_id, :with => :spec_customer_info
+    end
+  end
+
+  after :all do
+    SpecModel.cleanup!
+  end
+
+  describe "common behavior" do
+    before do
+      @info = SpecCustomerInfo.create :spec_customer_id => 1, :spec_person_name_id => 1, :is_current => true
+    end
+
+    it "should revert changes if exception is raised" do
+      old_id = @info.id
+      suppress(Exception) do
+        expect {
+          @info.update_attributes :spec_customer_id => nil, :spec_person_name_id => 2
+        }.not_to change(SpecCustomerInfo, :count)
+        @info.new_record?.should be_false
+        @info.id.should == old_id
+      end
+    end
+  end
+
+  describe 'Aggregated' do
+    before do
+      @existing = SpecPersonName.create! :first_name => 'John', :last_name => 'Smith'
+    end
+
+    it "should not duplicate records" do
+      expect {
+        SpecPersonName.create :first_name => 'John', :last_name => 'Smith'
+      }.not_to change(SpecPersonName, :count)
+    end
+
+    it "should lookup self and replace id with existing on create" do
+      name = SpecPersonName.new :first_name => 'John', :last_name => 'Smith'
+      name.save
+      name.id.should == @existing.id
+    end
+
+    it "should create a new record if lookup fails" do
+      expect {
+        SpecPersonName.create :first_name => 'Alice', :last_name => 'Smith'
+      }.to change(SpecPersonName, :count).by(1)
+    end
+
+    it "should lookup self and replace id with existing on update" do
+      name = SpecPersonName.create :first_name => 'Alice', :last_name => 'Smith'
+      name.update_attributes :first_name => 'John'
+      name.id.should == @existing.id
+    end
+  end
+
+  describe 'Tracked' do
+    before do
+      @customer = SpecCustomer.create
+      @info = @customer.create_spec_customer_info :spec_person_name_id => 1
+    end
+
+    it "should create a new current record if updated" do
+      expect {
+        @info.update_attributes(:spec_person_name_id => 2)
+      }.to change(SpecCustomerInfo, :count).by(1)
+    end
+
+    it "should replace itself with new id" do
+      old_id = @info.id
+      @info.update_attributes(:spec_person_name_id => 2)
+      @info.id.should_not == old_id
+    end
+
+    it "should set is_current record to false for superseded record" do
+      old_id = @info.id
+      @info.update_attributes(:spec_person_name_id => 2)
+      SpecCustomerInfo.find(old_id).is_current.should be_false
+    end
+
+    it "assigning a new current record should change is_current to false for previous one" do
+      new_info = SpecCustomerInfo.new :spec_person_name_id => 2, :is_current => true
+      @customer.spec_customer_info = new_info
+      new_info.spec_customer_id.should == @customer.id
+      @info.is_current.should be_false
+    end
+
+    describe 'with Aggregated' do
+      before do
+        @info.spec_person_name = SpecPersonName.create(:first_name => 'John', :last_name => 'Smith')
+        @info.save
+        @info.reload
+      end
+
+      it "should supersede when nested record changes" do
+        old_id = @info.id
+        @customer.spec_customer_info.spec_person_name.first_name = 'Alice'
+        expect{ @customer.save }.to change(@info, :spec_person_name_id)
+        @info.id.should_not == old_id
+        @info.is_current.should == true
+        SpecCustomerInfo.find(old_id).is_current.should be_false
+      end
+    end
+  end
+
+  describe 'Delegations' do
+    before do
+      @customer = SpecCustomer.create(
+        :spec_customer_info_attributes => {
+          :spec_person_name_attributes => {:first_name => 'John', :last_name => 'Smith'} } )
+      @info = @customer.spec_customer_info
+    end
+
+    it "should delegate methods to aggregated parts" do
+      @info.should respond_to(:first_name)
+      @info.should respond_to(:spec_suffix)
+      @info.last_name.should == 'Smith'
+    end
+
+    it "should delegate methods to representation" do
+      @customer.should respond_to(:first_name)
+      @customer.should respond_to(:spec_suffix)
+      @customer.last_name.should == 'Smith'
+      @customer.should respond_to(:custom_field)
+      @customer.should respond_to(:custom_field=)
+    end
+
+    it "should raise NoMethodError if unknown method received" do
+      expect{ @customer.impossibru }.to raise_error(NoMethodError)
+    end
+  end
+end
