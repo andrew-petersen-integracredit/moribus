3a3aabc1403726f77c47964fbb6122274f053934
Petr Savichev (proton) <psavichev@gmail.com>
2022-08-19 12:03:53 -0500

    update info's updated_at on is_current falsing

--- lib/moribus/extensions/has_current_extension.rb
+++ lib/moribus/extensions/has_current_extension.rb
@@ -6,15 +6,27 @@ module Moribus
       # Sets 'is_current' flag of overridden record to false, instead
       # of deleting it or setting foreign key to nil.
       def remove_target!(*)
+        # Use custom update to avoid running ActiveRecord optimistic locking
+        # and to avoid updating lock_version column:
+        klass = target.class
+
         if target.new_record?
           target.is_current = false
+          target.updated_at = Time.zone.now if klass.column_names.include?("updated_at")
         else
-          # Use custom update to avoid running ActiveRecord optimistic locking
-          # and to avoid updating lock_version column:
-          klass = target.class
-
           sql =  "UPDATE #{klass.quoted_table_name} " \
                  "SET \"is_current\" = #{klass.connection.quote(false)} "
+
+          if klass.column_names.include?("updated_at")
+            now = case ActiveRecord::Base.connection.adapter_name
+                  when "SQLite"     then "datetime('now')"
+                  # :nocov:
+                  when "PostgreSQL" then "NOW()"
+                  # :nocov:
+                  end
+            sql << %{, "updated_at" = #{now} } if now
+          end
+
           sql << "WHERE #{klass.quoted_primary_key} = " \
                  "#{klass.connection.quote(target.send(klass.primary_key))} "
 
