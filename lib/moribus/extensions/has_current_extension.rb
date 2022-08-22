870a6b044e9838da76a9664cef738ad4cec39721
Petr Savichev (proton) <psavichev@gmail.com>
2022-08-22 11:43:45 -0500

    code review fixes

--- lib/moribus/extensions/has_current_extension.rb
+++ lib/moribus/extensions/has_current_extension.rb
@@ -8,30 +8,26 @@ module Moribus
       def remove_target!(*)
         # Use custom update to avoid running ActiveRecord optimistic locking
         # and to avoid updating lock_version column:
-        klass = target.class
-
         if target.new_record?
           target.is_current = false
-          target.updated_at = Time.zone.now if klass.column_names.include?("updated_at")
+          target.updated_at = Time.zone.now if has_updated_at_column?
         else
+          klass = target.class
+
           sql =  "UPDATE #{klass.quoted_table_name} " \
                  "SET \"is_current\" = #{klass.connection.quote(false)} "
 
-          if klass.column_names.include?("updated_at")
-            now = case ActiveRecord::Base.connection.adapter_name
-                  when "SQLite"     then "datetime('now')"
-                  # :nocov:
-                  when "PostgreSQL" then "NOW()"
-                  # :nocov:
-                  end
-            sql << %{, "updated_at" = #{now} } if now
-          end
+          sql << %{, "updated_at" = #{klass.connection.quote(Time.zone.now)} } if has_updated_at_column?
 
           sql << "WHERE #{klass.quoted_primary_key} = " \
                  "#{klass.connection.quote(target.send(klass.primary_key))} "
 
           klass.connection.update sql
         end
+      end
+
+      private def has_updated_at_column?
+        target.class.column_names.include?("updated_at")
       end
     end
   end
